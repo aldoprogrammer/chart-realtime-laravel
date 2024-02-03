3f2d12d408152aed77a855b02ad043480ad34444
aldoprogrammer <aldobesma@gmail.com>
2024-02-04 03:11:07 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -444,6 +444,19 @@
                         </div>
                     </div>
 
+                    <div class="card">
+                        <div class="card-header ui-sortable-handle" style="cursor: move;">
+                            <canvas id="horizontalBar"></canvas>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header ui-sortable-handle" style="cursor: move;">
+                            <canvas id="pieChart"></canvas>
+                        </div>
+                    </div>
+
+
                 </section>
 
 
@@ -1140,6 +1153,58 @@
 
     <Script>
         $(function() {
+
+            // Pie Chart
+            //pie
+  var ctxP = document.getElementById("pieChart").getContext('2d');
+  var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+      labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+      datasets: [{
+        data: [300, 50, 100, 40, 120],
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+
+            // Chart bootstrap
+            new Chart(document.getElementById("horizontalBar"), {
+                "type": "horizontalBar",
+                "data": {
+                    "labels": ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Grey"],
+                    "datasets": [{
+                        "label": "My First Dataset",
+                        "data": [22, 33, 55, 12, 86, 23, 14],
+                        "fill": false,
+                        "backgroundColor": ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)",
+                            "rgba(255, 205, 86, 0.2)", "rgba(75, 192, 192, 0.2)",
+                            "rgba(54, 162, 235, 0.2)",
+                            "rgba(153, 102, 255, 0.2)", "rgba(201, 203, 207, 0.2)"
+                        ],
+                        "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)",
+                            "rgb(255, 205, 86)",
+                            "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)",
+                            "rgb(201, 203, 207)"
+                        ],
+                        "borderWidth": 1
+                    }]
+                },
+                "options": {
+                    "scales": {
+                        "xAxes": [{
+                            "ticks": {
+                                "beginAtZero": true
+                            }
+                        }]
+                    }
+                }
+            });
+
             // jvectormap data
             var visitorsData = {
                 US: 398, // USA
